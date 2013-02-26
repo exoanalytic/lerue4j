54ffdd47cc1e0599961c883e372646cb567d8039
Joey Mink <jmink@exoanalytic.com>
2013-02-26 08:44:10 -0500

    Add missing exception class

---
+++ src/main/java/com/exoanalytic/smartystreets/list/LiveAddressForListsException.java
@@ -0,0 +1,14 @@
+package com.exoanalytic.smartystreets.list;
+
+@SuppressWarnings("serial")
+public class LiveAddressForListsException extends Exception {
+
+	public LiveAddressForListsException(String msg) {
+		super(msg);
+	}
+
+	public LiveAddressForListsException(Throwable t) {
+		super(t);
+	}
+
+}
